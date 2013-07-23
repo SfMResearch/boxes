95f29bba770b379b306058f5a713dba5047b12a1
Michael Tremer <michael@tremer.info>
2013-07-23 12:09:43 +0200

    Show version information about OpenCV.
    Also check for all available hardware support and show that
    in the version string output.

--- src/lib/boxes.cc
+++ src/lib/boxes.cc
@@ -1,4 +1,8 @@
 
+#include <list>
+#include <opencv2/opencv.hpp>
+#include <tuple>
+
 #include <boxes/boxes.h>
 
 namespace Boxes {
@@ -21,6 +25,31 @@ namespace Boxes {
 		str += __DATE__;
 		str += " ";
 		str += __TIME__;
+		str += ") ";
+
+		// OpenCV
+		str += "OpenCV ";
+		str += CV_VERSION;
+
+		// List active optimizations.
+		std::list<std::tuple<std::string, int>> hardware_support;
+		hardware_support.push_back(std::tuple<std::string, int>("MMX",      CV_CPU_MMX));
+		hardware_support.push_back(std::tuple<std::string, int>("SSE",      CV_CPU_SSE));
+		hardware_support.push_back(std::tuple<std::string, int>("SSE2",     CV_CPU_SSE2));
+		hardware_support.push_back(std::tuple<std::string, int>("SSE3",     CV_CPU_SSE3));
+		hardware_support.push_back(std::tuple<std::string, int>("SSSE3",    CV_CPU_SSSE3));
+		hardware_support.push_back(std::tuple<std::string, int>("SSE4.1",   CV_CPU_SSE4_1));
+		hardware_support.push_back(std::tuple<std::string, int>("SSE4.2",   CV_CPU_SSE4_2));
+		hardware_support.push_back(std::tuple<std::string, int>("POPCOUNT", CV_CPU_POPCNT));
+		hardware_support.push_back(std::tuple<std::string, int>("AVX",      CV_CPU_AVX));
+
+		str += " (HW Support:";
+		for (std::list<std::tuple<std::string, int>>::const_iterator i = hardware_support.begin(); i != hardware_support.end(); ++i) {
+			if (cv::checkHardwareSupport(std::get<1>(*i))) {
+				str += " ";
+				str += std::get<0>(*i);
+			}
+		}
 		str += ")";
 
 		return str;
