cae1b3aabb444c5dd0f3e31c82e5d8939c1d9b98
Michael Tremer <michael@tremer.info>
2013-08-15 11:23:21 +0200

    boxes.h: Explicitely include everything.

--- include/boxes.h
+++ include/boxes.h
@@ -2,7 +2,13 @@
 #ifndef BOXES_H
 #define BOXES_H
 
-#include "boxes/boxes.h"
-#include "boxes/image.h"
+#include <boxes/boxes.h>
+
+#include <boxes/camera_matrix.h>
+#include <boxes/constants.h>
+#include <boxes/feature_matcher.h>
+#include <boxes/feature_matcher_optical_flow.h>
+#include <boxes/image.h>
+#include <boxes/point_cloud.h>
 
 #endif
