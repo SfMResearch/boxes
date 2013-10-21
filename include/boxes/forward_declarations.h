ce5584884a8d24e4949410c0a797d6bcf39232cc
Michael Tremer <michael@tremer.info>
2013-10-21 15:41:28 +0200

    Add a header file with forward declaration.
    This is needed to resolve some recursive header includes.

---
+++ include/boxes/forward_declarations.h
@@ -0,0 +1,10 @@
+
+#ifndef BOXES_FORWARD_DECLARATIONS_H
+#define BOXES_FORWARD_DECLARATIONS_H
+
+namespace Boxes {
+	class Image;
+	class PointCloud;
+};
+
+#endif /* BOXES_FORWARD_DECLARATIONS_H */
